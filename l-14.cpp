fe5a90117e5ad2ed96ab0ab84d4a9580f3d8c20a
Annie <annmora21@gmail.com>
2025-09-25 20:53:37 -0700

    v-0.3

--- l-14.cpp
+++ l-14.cpp
@@ -14,15 +14,22 @@ private:
 
 public:
 // Getters and Setters
-int r()     {return red;}
-int g()     {return green;}
-int b()     {return blue;}
+int r()             { return red; }
+void setR(int r)    { red = r; }
+
+int g()             { return green; }
+void setG(int g)    { green = g; }
+
+int b()             { return blue; }
+void setB(int b)    {blue = b;}
 };
 
 int main()
 {
-    // Color objects
+    // Color objects r,g,b
     Color r;
+    //Color.setR(255,0,0);
+
     Color g;
     Color b;
     
