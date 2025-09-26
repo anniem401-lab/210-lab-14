79ce054181e7b6ff878e47796bd0aac70504c922
Annie <annmora21@gmail.com>
2025-09-25 20:23:09 -0700

    v-0.2

--- l-14.cpp
+++ l-14.cpp
@@ -13,24 +13,18 @@ private:
     int blue;
 
 public:
-    Color(int r, int g, int b) : red(r), green(g), blue(b) {}
-    void display() const 
-    {
-        cout << "Red: " << red << ", Green: " << green << ", Blue: " << blue << endl;
-    }
+// Getters and Setters
+int r()     {return red;}
+int g()     {return green;}
+int b()     {return blue;}
 };
 
 int main()
 {
-    // Create Color objects
-    Color color1(255, 0, 0);  // Red
-    Color color2(0, 255, 0);  // Green
-    Color color3(0, 0, 255);  // Blue
-
-    // Display the colors 
-	color1.display();
-	color2.display();
-	color3.display();
-
+    // Color objects
+    Color r;
+    Color g;
+    Color b;
+    
 	return 0;
 }
